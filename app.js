946ea8cdec9dcfbb0ce1023b3024c380c68db5ea
nathanpepin <46303125+nathanpepin@users.noreply.github.com>
2020-05-20 22:48:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var admin = require('firebase-admin');
+const express = require('express');
+const admin = require('firebase-admin');
 
 const app = express();
-
-// TODO: Enter the path to your service account json file
-// Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
-// TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
 
 const serviceAccount = require("./tcp-madden-firebase-adminsdk-ulhkb-5fe865d55d.json");
 
